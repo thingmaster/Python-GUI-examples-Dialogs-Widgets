13464eb81e90b86092a88460c01a4f8af524adf6
thingmaster <46053711+thingmaster@users.noreply.github.com>
2018-12-21 11:07:59 -0800

    Add files via upload
    Initial MG git version of python tkinter dialog box examples (MGdialogs.py)
    Initial MG git version  of python tkinter gui widgets (checkbox, menu, radiobutton, dynamic (add to it) pulldown list

---
+++ MGDialogs.py
@@ -0,0 +1,179 @@
+# Copyright @ 2018 Michael George
+#
+# some examples of putting simple controls on a main window, popup from a button click
+# also pops up an About Box dialog
+#
+#   Python Compability:     Developed on IDLE@Python 3.7.1.
+#   Platform Compatibilty:  recent testing on MacOS, Win10
+#
+#   mg 2018-12-21 initial upload to git
+#
+import tkinter as tk
+from tkinter import Tk, OptionMenu, Label, Button, Checkbutton, Radiobutton, Scrollbar, Text, Entry, StringVar, DISABLED, NORMAL, END, RIGHT, LEFT, W, IntVar, END, W, E, Y
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
+
+# basic aboutdialog exits on click of OK
+# 
+class AboutDialog:
+    def __init__(self, parent):
+        self.myparent = parent
+        self.string1 = None
+        # create the new toplevel window frame for this dialog
+        # we been this window instance  'top' for creating widgets and quitting the dialog
+        if parent:
+            top = self.top = tk.Toplevel(parent)
+        else:
+            top = self.top = tk.Tk()
+        #centerinwindow(top,700, 200)
+        top.configure(background='green')
+        top.lift()
+        self.myLabel = tk.Label(top, text='    MG File Utilities Version 3.0 2028-12-20    \n ',
+                                font='-*-helvetica-*-r-*-*-*-180-*-*-*-*-*-*')
+        self.myLabel.grid(column=8, row=1, sticky=W+E)
+        self.myLabel0 = tk.Label(top, text='    Copyright @2018 Michael George')
+                                #font='-*-helvetica-*-r-*-*-*-180-*-*-*-*-*-*')
+        self.myLabel0.grid(column=8, row=2, sticky=W+E)
+        self.myLabel1 = tk.Label(top, text=' ',
+                                font='-*-helvetica-*-r-*-*-*-180-*-*-*-*-*-*')
+        self.myLabel1.grid(column=9, row=3, sticky=W+E)
+        self.myLabel2 = tk.Label(top, text=' ',
+                                font='-*-helvetica-*-r-*-*-*-180-*-*-*-*-*-*')
+        self.myLabel2.grid(column=9, row=0, sticky=W+E)
+
+
+        self.startbutton2text = StringVar()
+        self.startbutton2text.set("OK")
+        self.start_button2 = Button(top, textvariable=self.startbutton2text, command=self.onClick)
+        #, font='-*-helvetica-*-r-*-*-*-180-*-*-*-*-*-*')
+        self.start_button2.grid(column=8, row=6 ) #, sticky=W+E)
+        self.myLabel3 = tk.Label(top, text=' ',
+                                font='-*-helvetica-*-r-*-*-*-180-*-*-*-*-*-*')
+        self.myLabel3.grid(column=9, row=7, sticky=W+E)
+
+    #  dialog OK button click; closes the AboutDialog window
+    def onClick(self):
+        self.top.destroy() #Destroy()
+        return
+    
+# test dialog example
+class MyDialog:
+    def __init__(self, parent):
+        self.myparent = parent
+        self.string1 = None
+        top = self.top = tk.Toplevel(parent)
+        self.myLabel = tk.Label(top, text='Enter your username below')
+        self.myLabel.pack()
+
+        self.myEntryBox = tk.Entry(top)
+        self.myEntryBox.pack()
+
+        #self.mySubmitButton = tk.Button(top, text='Submit', command=self.send)
+        #self.mySubmitButton.pack()
+
+        self.startbutton2text = StringVar()
+        self.startbutton2text.set("Scan secondary tgt")
+        self.start_button2 = Button(top, textvariable=self.startbutton2text, command=lambda: self.update("start2")).pack()
+
+        self.startbutton3text = StringVar()
+        self.startbutton3text.set("askFolder")
+        self.start_button3 = Button(top, textvariable=self.startbutton3text, command=lambda: self.update("askfolder")).pack()
+
+        self.start_button4 = Button(top, text="AskFile", command=lambda: self.update("askfile")).pack()
+
+        self.buttonok = Button(top, text="ok", command=lambda: self.onok()).pack()
+        self.buttoncancel= Button(top, text="cancel", command=lambda: self.oncancel()).pack()
+
+    def update(self,msg):
+        if msg=='askfolder':
+            newstr = filedialog.askdirectory() #self.browsefilebuttonservice()
+            print(newstr)
+        elif msg=='askfile':
+            newstr = self.browsefilebuttonservice()
+            print(newstr)
+        else:
+            print(msg)
+        
+    def send(self):
+        #global username
+        #username = self.myEntryBox.get()
+        self.string1 = self.myEntryBox.get()
+
+    # closed MyDialog window
+    def oncancel(self):
+        self.top.destroy()
+        
+    def onok(self):
+        self.string1 = self.myEntryBox.get()
+        self.top.destroy()
+        return 
+
+    def browsefilebuttonservice(self):
+        file_opt = options =  {}
+        #MAC fails  options['filetypes'] = [('gzipped SOFT', '.soft.gz'), ('SOFT', '.soft'),('Comma Separated', '.csv')]
+        options['parent'] = self.myparent
+        options['initialdir'] = 'data'
+        options['title'] = "AHREA - Select data file."
+        fname = askopenfilename(**options)
+        #filetypes=[("JPG","*.jpg;*.jpeg"),
+#                                           ("Docs","*.pdf;*.doc;*.docx;*.xls;*.xlsx"),
+#                                           ("HTML files", "*.html;*.htm"),
+#                                           ("All files", ".*")])
+        if fname:
+            try:
+                print("Selected file: (%s)"%fname)
+                return fname
+            except:                     # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % fname)
+            return
+
+# helper function to center and size a window
+def centerinwindow(mywindow, wid, ht):
+    # do some window size and placement math
+    w = wid #400 # width for the Tk root
+    h = ht #350 # height for the Tk root
+    # get screen width and height
+    ws = mywindow.winfo_screenwidth() # width of the screen
+    hs = mywindow.winfo_screenheight() # height of the screen
+    # calculate x and y dimensions for the Tk root window
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    # set the dimensions of the window and set xy placement
+    mywindow.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+
+
+if __name__ == '__main__':
+    # click handler for the main window 'Click me' button events
+    # 
+    def onClick(rootwin):
+        inputDialog = MyDialog(rootwin)
+        rootwin.wait_window(inputDialog.top)
+        print('Username: ', inputDialog.string1)
+
+
+    #
+    # click handler for the main window 'Click me' button events
+    # closes the main / root window
+    # 
+    def onClickCancel(rootwin):
+        rootwin.destroy()
+
+    # construct a window with some controls and some actions with popup 
+    root = tk.Tk()
+    centerinwindow(root, 400,350)
+    # put the two controls on the main window
+    mainLabel = tk.Label(root, text='Example for pop up input box')
+    mainLabel.pack()
+    mainButton = tk.Button(root, text='Click me', command=lambda: onClick(root))
+    mainButton.pack()
+    mainButton = tk.Button(root, text='Cancel', command=lambda: onClickCancel(root))
+    mainButton.pack()
+
+    # after that is drawn, put up a modal AboutDialog
+    adialog = AboutDialog(root)
+    root.wait_window(adialog.top)
+
+
+    root.mainloop()
---
+++ MGwidgets.py
@@ -0,0 +1,300 @@
+# Copyright @ 2018 Michael George
+#
+# MGwidgets implements some basic widget classes for reuse
+# revisions:
+#    2018-12-06 mg initial version with main menu and a combo button with list of checkboxes
+#    2018-12-20 add the file menu. add the help menu. 
+#       remove local state vars from these widgets, use keyval pairs provided by caller to store menu & gui state values
+#       added binding example for widgets Enter Leave & flyover callback argument in the class
+#
+from tkinter import Tk, OptionMenu, Label, Button, Menu, Menubutton, Checkbutton, Radiobutton, Scrollbar, Text, Entry, StringVar
+from tkinter import RAISED, DISABLED, NORMAL, END, RIGHT, LEFT, W, IntVar, BooleanVar, END, W, E, Y
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
+import sys
+
+# MG Class for creating and managing a main menu bar with multiple command menus, check menus, radio menus
+# caller must provide
+#     the 'topwin' parent window handle and two keyvalue lists
+#     the keyvalcallbacks dict with named callbacks for every menu function,
+#     the keyvalstatevars dict with named variables for every variable menu state
+class mgmainmenu:
+    # init application menubar, the Dump Options menu and the Database Select options + another Info/help/about
+    # the callbacks are in a dictionary like "newfilefn":MGU_newfilefn ...
+    # the statevars are in a dictionary like "dumpfileopt":MGU_dumpfileoptval
+    # caller provides them... menu state changes update them/ call them
+    def __init__( self, topwin, keyvalcallbacks, keyvalstatevars ):
+        self.parent = topwin
+        self.menubar = Menu(self.parent)
+        self.keyvalstatevars = keyvalstatevars
+        #???self.view_menu = Menu(self.menubar)
+        # the file menu
+        Command_button = Menubutton(self.menubar, text='Simple Button Commands',  underline=0)
+        Command_button.menu = Menu(Command_button)
+        #Command_button.menu = Menu(self.menubar)
+        Command_button.menu.add_command(label="Undo")
+        # undo is the 0th entry...
+        Command_button.menu.entryconfig(0, state=DISABLED)
+        Command_button.menu.add_command(label='New...', underline=0, command=keyvalcallbacks["newfilefn"])
+        Command_button.menu.add_command(label='Open...', underline=0, command=keyvalcallbacks["openfilefn"])
+        # alternate font example
+        Command_button.menu.add_command(label='Print', underline=0,
+                                    font='-*-helvetica-*-r-*-*-*-180-*-*-*-*-*-*',
+                                    command=keyvalcallbacks["printmenufn"])
+        # separator example
+        Command_button.menu.add('separator')
+        # aLternate color example
+        Command_button.menu.add_command(label='Quit', underline=0,
+                                    background='red',
+                                    activebackground='green',
+                                    command=keyvalcallbacks["quitmenufn"])
+        self.menubar.add_cascade(label='File', menu=Command_button.menu)
+
+        #self.menubar.add_cascade(label='View', menu=self.view_menu)
+        # add a menubar menu to select option for dumping all elements, or only Dups or only Unique
+        self.dumpselect_menu = Menu(self.menubar)
+        self.dumpradioval= IntVar()
+        self.dumpradioval.set(1)
+        self.dumpselect_menu.add_radiobutton(label="Dump All", value=1,variable=keyvalstatevars['DumpOptions'])
+        self.dumpselect_menu.add_radiobutton(label="Dump only unique",value=2,variable=keyvalstatevars['DumpOptions'])
+        self.dumpselect_menu.add_radiobutton(label="Dump only Dups",value=3,variable=keyvalstatevars['DumpOptions'])
+        self.menubar.add_cascade(label='Dump File options', menu=self.dumpselect_menu)
+        #
+        # add a menubar menu to select All Databases or one database for dumping
+        self.dumpdbselect_menu = Menu(self.menubar)
+        self.dumpdbselectcount = 1
+        self.show_all = IntVar()
+        self.show_all.set(True)
+        self.dbselmenukv = { "Show all":self.show_all}
+        # menu begins with only one  option- dump all
+        self.dumpdbselect_menu.add_checkbutton(label="Show All", onvalue=True, offvalue=False, variable=self.show_all)
+        self.menubar.add_cascade(label='Dump DB selection', menu=self.dumpdbselect_menu)
+
+        #
+        # add a menubar menu for the app info Help/ about/ etc
+        self.vendorhelp_menu = Menu(self.menubar)
+        self.help001 = { "Help":self.show_all}
+        # menu begins with only one  option- dump all
+        self.vendorhelp_menu.add_checkbutton(label="Show _Hints", onvalue=True, offvalue=False, variable=keyvalstatevars['HintsFlag'])
+        self.vendorhelp_menu.add_checkbutton(label="Monitor volumes (10second timer activity)", onvalue=True, offvalue=False, variable=keyvalstatevars['TimerCheck'])
+        self.vendorhelp_menu.add_command(label='Help...', underline=0, command=keyvalcallbacks["helpmenufn"])
+        # separator example
+        self.vendorhelp_menu.add('separator')
+        self.vendorhelp_menu.add_command(label='Dbg...', underline=0, command=self.MGmenudumpdbg)
+        self.vendorhelp_menu.add_command(label='About...', underline=0, command=keyvalcallbacks["aboutmenufn"])
+        self.menubar.add_cascade(label='Help', menu=self.vendorhelp_menu)
+
+        # register all this with the callers parent window frame
+        self.parent.config(menu=self.menubar)
+
+    # add a menuitem to the check options in the database selection menu list
+    # this option allows the databases to be added as they are specified & scanned by user
+    # later used as criteria for which source data to include in the dump of memory database
+    def mgmenuitem_add(self, newlabel):
+        newkvint = IntVar()
+        newkvint.set(False)
+        self.dbselmenukv[newlabel] = newkvint
+        self.dumpdbselect_menu.add_checkbutton(label=newlabel, onvalue=True, offvalue=False, variable=newkvint)
+        self.dumpdbselectcount += 1
+
+    # get the menu selection for which databases to include in the dump
+    def mgmenuselections_dumpget(self):
+        return self.dbselmenukv
+
+    # get the menu radio selection for including All/Unique/Duplicate files in the dump
+    def mgmenuselection_dumpoptions_get(self):
+        return self.dumpradioval.get()
+
+    def MGmenugroups_get(self):
+        return self.mgmenuselection_dumpoptions_get(), self.mgmenuselections_dumpget()
+
+    # tutorial dump of the state of the menu vars dictionary
+    def MGmenudumpdbg(self):
+        for i, (k,v) in enumerate(self.keyvalstatevars.items()):
+            print(i, k,  v.get())
+
+#
+# an MG Class for creating & interacting with a button that has check items
+#  includes a _get function returning keyvalue pairs for all check items' states
+#  includes an _add method adding the item you request (note if it's not unique you only get key-value info for 1st)
+#
+class mgcombocheckboxes:
+    def __init__( self, topwin, itemkeyval):
+        self.parentframe=topwin
+        self.mb = Menubutton ( topwin, text="CheckComboBox", relief=RAISED )
+        self.mb.grid()            
+        self.mb.menu  =  Menu( self.mb, tearoff = 0 )
+        self.mb["menu"] = self.mb.menu
+        #
+        #self.mb.menu["menu"] = self.mb.menu
+        self.itemischecked = {}
+        self.comboitemcount = 0
+        for menuentry in itemkeyval:
+            itemvar = IntVar()
+            itemvar.set(itemkeyval[menuentry])
+            # a keyvalue pair with menu item name and an itemvar unique to that selection
+            self.itemischecked[menuentry] = itemvar
+            self.mb.menu.add_checkbutton ( label=menuentry, variable=itemvar)
+            self.comboitemcount += 1
+        #self.mb.config(menu=self.mb.menu)
+            
+    def grid(self, row, column):
+        #self.mb.grid(row=5, column=7)
+        pass
+
+    #return the KV pairs of the combobuttons true/false checked or not
+    def mgcombochecks_get( self ):
+        return self.itemischecked
+    
+    def mgcombochecks_add( self, itemstr ):
+            itemvar = IntVar()
+            itemvar.set(False)
+            # a keyvalue pair with menu item name and an itemvar unique to that selection
+            self.itemischecked[itemstr] = itemvar
+            self.mb.menu.add_checkbutton ( label=itemstr, variable=itemvar)
+            self.comboitemcount +=1 
+# MG class for managing all the radio groups in a window
+class MGradiogroup():
+    # init the instance of MGradiogroup( parentwin, kvgrouppairs, flyover_entryfn)
+    #  kvgrouppairs is the keyvalue pairs with variables that are associated with the radio options
+    # example of full implementation in MGGuiUtils.py - see MGradiogroup instantiation
+    #
+    def __init__(self, parentwin, kvgrouppairs, flyoverfn):
+        # CSV Generation options radio button
+        self.master = parentwin
+        self.kvgrouppairs = kvgrouppairs
+        self.flyoverfn = flyoverfn
+
+        # first radio group; note that command= callback is not needed. add it if you want to process clicks as they happen. my app just gets the state from vars
+        Radiobutton(parentwin, text = "All Files", variable=kvgrouppairs['DumpOptions'],  value = 1).grid(row=12, column=4,sticky=W) #command=self.MGradiogroup_fn,
+        Radiobutton(parentwin, text = "Exclude Dups", variable=kvgrouppairs['DumpOptions'], value=2).grid(row=12, column=5,sticky=W)#command=self.MGradiogroup_fn,
+        Radiobutton(parentwin, text = "Only Dups", variable=kvgrouppairs['DumpOptions'],value=3).grid(row=12,column=6, sticky=W)#command=self.MGradiogroup_fn,
+        # second radio group
+        Radiobutton(parentwin, text = "All DBs", variable=kvgrouppairs['DBOptions'], value=1).grid(row=13,column=4, sticky=W)
+        Radiobutton(parentwin, text = "Only in selected DB", variable=kvgrouppairs['DBOptions'], value=2).grid(row=13,column=5, sticky=W)
+        Radiobutton(parentwin, text = "Not in selected DB", variable=kvgrouppairs['DBOptions'], value=3).grid(row=13,column=6, sticky=W)
+        # third radio group
+        #init the db from file or folder  radio button)
+        rb1 = Radiobutton(parentwin, text = "Load\nCSV", variable=self.kvgrouppairs['CsvOrLive'], value = 1) #command=self.inittypeFn, 
+        rb1.grid(row=1, column=5,sticky=W)
+        rb2 = Radiobutton(parentwin, text = "Scan\nTree", variable=self.kvgrouppairs['CsvOrLive'], value = 2) #, command=self.inittypeFn
+        rb2.grid(row=1, column=6,sticky=W)
+        rb1.bind("<Enter>",lambda event:self.flyoverfn("Choose LOAD CSV to load memory database from an existing .CSV file database (dumped from previous filesystem scan)\n"+
+                                                               "This may be useful for checking a new filesystem against your existing set of files where:\n"+
+                                                               "  a) you don't want to RE-scan your base files which may be huge list and take hours\n"+
+                                                               "  b) you don't have direct access to the backup, or it is very slow network access"))
+        rb1.bind("<Leave>",lambda event:self.flyoverfn(None))
+        rb2.bind("<Enter>",lambda event:self.flyoverfn("Choose SCAN TREE to load memory database from a target filesystem in the current system\n\n\n"))
+        rb2.bind("<Leave>",lambda event:self.flyoverfn(None))
+
+    # anyone with the instance can get the state of the two radio groups. don't really need this either except for tutorial app.
+    # in a real application, the instantiating caller provides these menu-associated variables so THEY RESIDE IN THE CALLER'S CONTEXT - caller can access directly
+    def MGradiogroup_get(self):
+        print("radiogroupget", self.kvgrouppairs['DumpOptions'].get(), self.kvgrouppairs['DBOptions'].get())
+        return self.kvgrouppairs['DumpOptions'].get(), self.kvgrouppairs['DBOptions'].get()
+
+    # callback on clicks to show the state of the two radio groups. don't really need this for app but it's a good tutorial helper 
+    def MGradiogroup_fn(self):
+        print("dumpopts", self.kvgrouppairs['DumpOptions'].get())
+        print("dbopts", self.kvgrouppairs['DBOptions'].get())
+        return
+
+# mg class for managing all the checkboxes in a window
+class MGcheckboxes():
+    # init the instance of MGcheckboxes( parentwin, kvgrouppairs, flyover_entryfn)
+    #  kvgrouppairs is the keyvalue pairs with variables that are associated with the checkbox options
+    # example of full implementation in MGGuiUtils.py - see MGradiogroup instantiation
+    #
+    def __init__(self, parentwin, kvgrouppairs, flyoverfn):
+        # CSV Generation options check button
+        self.master = parentwin
+        self.kvgrouppairs = kvgrouppairs
+        self.flyoverfn = flyoverfn
+
+        # the checkbox gui items to enable / disable various optional activities
+        self.delbutton = Checkbutton(parentwin, text="Delete\nselected", variable=self.kvgrouppairs['DeleteCheck'], onvalue=True, offvalue=False).grid(row=21, column=4, columnspan=1, sticky=W)
+        self.copycheckbutton = Checkbutton(parentwin, text="Copy\nselected", variable=self.kvgrouppairs['CopyCheck'], onvalue=True, offvalue=False).grid(row=21, column=3, columnspan=1, sticky=W)
+        self.printcheckbutton = Checkbutton(parentwin, text="Print CSV recs", variable=self.kvgrouppairs['PrintCheck'], onvalue=True, offvalue=False).grid(row=21, column=2,columnspan=1, sticky=W)
+        self.dupcheckbutton = Checkbutton(parentwin, text="Dup checking", variable=self.kvgrouppairs['DupCheck'], onvalue=True, offvalue=False).grid(row=21, column=1,columnspan=1, sticky=W)
+        # note that we don't need to keep the handle to the check buttons so they are not instance self.xx variables
+        timercheckbutton = Checkbutton(parentwin, text="Timer enable", variable=self.kvgrouppairs['TimerCheck'], onvalue=True, offvalue=False)
+        timercheckbutton.grid(row=21, column=0,columnspan=1, sticky=W)
+        timercheckbutton.bind("<Enter>",lambda event:self.flyoverfn("You can turn the timer off so that timer won't monitor insertion/removal automatically\n\n\n"))
+        timercheckbutton.bind("<Leave>",self.flyoverfn(None))
+
+    # anyone with the instance can get the state of the two radio groups. don't really need this either except for tutorial app.
+    # in a real application, the instantiating caller provides these menu-associated variables so THEY RESIDE IN THE CALLER'S CONTEXT - caller can access directly
+    def MGradio_get(self):
+        return 
+
+    # callback on clicks to show the state of the two radio groups. don't really need this for app but it's a good tutorial helper 
+    def MGcheck_fn(self):
+        return
+
+  
+if __name__ == '__main__':
+    # init a root window var to reference in the quit button
+    myroot = None
+    def cbfunc():
+        print("example callback fn")
+    def quitfunc():
+        print("quit")
+        myroot.destroy()
+
+    # display hints when mouse <Entry> events get us here
+    # clear hints when <Leave> events get us here
+    # the menu has a check item to turn the behavior on/ off
+    def flyoverfunc(str):
+        # no hints if the GUI / menu flag is not checked
+        if not menustatevars['HintsFlag'].get():
+            return
+        if str:
+            print("flyover",str)
+        
+    def Item_test(comboinstance,menuinstance):
+        mychks= comboinstance.mgcombochecks_get()
+        for myitem in mychks:
+            print( myitem, "state=", mychks[myitem].get())
+        menudata = menuinstance.mgmenuselections_dumpget()
+        for mdbsel in menudata:
+         print("menudbsel", mdbsel, menudata[mdbsel].get())
+         #.show_all.get(),menuinstance.show_done.get())
+        print("menudump",menuinstance.dumpradioval.get())
+        # add more items to a menu
+        menuinstance.mgmenuitem_add("another item:%1d"%menuinstance.dumpdbselectcount)
+        # add more check items to the combo button
+        comboinstance.mgcombochecks_add( "another button menu item:%1d"%comboinstance.comboitemcount)
+
+    myroot = Tk()
+    menucallbacks = { "newfilefn":cbfunc, "openfilefn":cbfunc,
+            "printmenufn":cbfunc, "helpmenufn":cbfunc,
+            "aboutmenufn":cbfunc, "quitmenufn":quitfunc,
+            "dumpoptsfn":cbfunc }
+    menustatevars = {'DumpOptions':IntVar(), 'HintsFlag':BooleanVar(), 'DeleteCheck':BooleanVar(), 'DupCheck':BooleanVar(),
+                          'CopyCheck':BooleanVar(),
+                          'PrintCheck':BooleanVar(), 'TimerCheck':BooleanVar(),'DBOptions':IntVar(), 'CsvOrLive':IntVar()}
+    menustatevars['DumpOptions'].set(1)
+    menustatevars['DBOptions'].set(1)
+    menustatevars['DumpOptions'].set(1)
+    menustatevars['HintsFlag'].set(False)
+    menustatevars['DeleteCheck'].set(False)
+    menustatevars['DupCheck'].set(False)
+    menustatevars['CopyCheck'].set(False)
+    menustatevars['PrintCheck'].set(True)
+    menustatevars['TimerCheck'].set(True)
+    menustatevars['CsvOrLive'].set(2)
+
+    mgmenu = mgmainmenu(myroot,menucallbacks,menustatevars )
+    mgcombo1 = mgcombocheckboxes( myroot, {'Item0':False,'Item1':False,'Item2':True,'Item3':True})
+    mgcombo1.grid(6, 1)
+    button1 = Button(myroot, text="Item True/False Test", command = lambda: Item_test(mgcombo1,mgmenu))
+    button1.grid(row=5, column=6)
+
+    #create the radio button group via class in MGwidgets.py
+    radios = MGradiogroup(myroot, menustatevars, flyoverfunc)
+    checks = MGcheckboxes(myroot, menustatevars, flyoverfunc)
+    
+
+    myroot.title( "MGwidgets-3.8-002-122118")
+    myroot.mainloop()
